c1fef5fc2b5d8b0331c1364fe217727ca8f59db3
tofmat <tofmatt@gmail.com>
2020-02-07 02:10:40 +0100

    installed a nice notification system

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="row my-5">
-        <div class="col-md-6 offset-md-3">
+    
+
+ <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Upload Photos
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Upload Images</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
         <div class="card mb-5">
         <img src="../assets/upload.png" class="card-img-top image" alt="" width="200px" height="300px">
         <hr>
@@ -14,7 +31,11 @@
   </div>
 </div>
  </div>
+      </div>
     </div>
+  </div>
+</div>
+    
 </template>
 
 <style scoped>
