a80e3698b03e3590c20c4e453f8b609d57830941
tofmat <tofmatt@gmail.com>
2020-02-08 16:18:04 +0100

    quater final commit

--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -13,7 +13,7 @@
              class="img-fluid img-thumbnail" alt="" />
          <div class="card-body">
             <p class="card-text text-center">
-              <a href="#">Download</a> | <a href="#">Share</a>
+              <button href="#">Download</button> | <button href="#">Share</button>
             </p>
           </div>
       </div>
@@ -29,7 +29,8 @@ export default {
   mounted() {
     console.log(this.photos);
   },
-
-  methods: {}
-};
+  methods: {
+    
+  }
+}
 </script>
